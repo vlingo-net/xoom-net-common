ad43d08c582288e3813515a45426e8367b840c95
Zp Bappi <zpbappi@gmail.com>
2018-05-21 21:07:48 +1000

    added empty test project

---
+++ src/Vlingo.Common.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Vlingo.Common.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

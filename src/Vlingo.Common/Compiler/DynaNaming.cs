9cdfe515161d62702ea96ce0caadcada3fee16ce
Zp Bappi <zpbappi@gmail.com>
2018-06-18 22:38:32 +1000

    fixed a bug and add method overload for DynaNaming methods

--- src/Vlingo.Common/Compiler/DynaNaming.cs
+++ src/Vlingo.Common/Compiler/DynaNaming.cs
@@ -1,21 +1,39 @@
-﻿namespace Vlingo.Common.Compiler
+﻿using System;
+
+namespace Vlingo.Common.Compiler
 {
     public static class DynaNaming
     {
         public static string ClassNameFor<T>(string postfix)
+            => ClassNameFor(typeof(T), postfix);
+
+        public static string ClassNameFor(Type type, string postfix)
         {
-            var type = typeof(T);
             var className = type.Name;
 
-            if(type.IsInterface && type.Name.StartsWith('I'))
+            if (type.IsInterface && type.Name.StartsWith('I'))
             {
                 className = className.Substring(1);
+            }
+            if (type.IsGenericType)
+            {
+                className = className.Substring(0, className.IndexOf('`'));
             }
 
             return $"{className}{postfix}";
         }
 
         public static string FullyQualifiedClassNameFor<T>(string postfix)
-            => $"{typeof(T).Namespace}.{ClassNameFor<T>(postfix)}";
+            => FullyQualifiedClassNameFor(typeof(T), postfix);
+
+        public static string FullyQualifiedClassNameFor(Type type, string postfix)
+        {
+            if (string.IsNullOrWhiteSpace(type.Namespace))
+            {
+                return ClassNameFor(type, postfix);
+            }
+
+            return $"{type.Namespace}.{ClassNameFor(type, postfix)}";
+        }
     }
 }

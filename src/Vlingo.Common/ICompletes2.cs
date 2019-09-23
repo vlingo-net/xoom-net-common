e5f8304fd209d85b92cfb4cb57648b2ba505a056
Tomasz Jaskula <tjaskula@la-francaise.com>
2019-09-23 14:20:22 +0200

    Add ICompletes2 for new implementation tests

---
+++ src/Vlingo.Common/ICompletes2.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) 2012-2019 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using System;
+
+namespace Vlingo.Common
+{
+    public interface ICompletes2<TResult> : ICompletes
+    {
+        ICompletes2<TNewResult> AndThen<TNewResult>(TimeSpan timeout, TNewResult failedOutcomeValue, Func<TResult, TNewResult> function);
+        ICompletes2<TO> AndThen<TO>(TO failedOutcomeValue, Func<TResult, TO> function);
+        ICompletes2<TO> AndThen<TO>(TimeSpan timeout, Func<TResult, TO> function);
+        ICompletes2<TO> AndThen<TO>(Func<TResult, TO> function);
+
+        ICompletes2<TResult> AndThenConsume(TimeSpan timeout, TResult failedOutcomeValue, Action<TResult> consumer);
+        ICompletes2<TResult> AndThenConsume(TResult failedOutcomeValue, Action<TResult> consumer);
+        ICompletes2<TResult> AndThenConsume(TimeSpan timeout, Action<TResult> consumer);
+        ICompletes2<TResult> AndThenConsume(Action<TResult> consumer);
+
+        TO AndThenTo<TF, TO>(TimeSpan timeout, TF failedOutcomeValue, Func<TResult, TO> function);
+        TO AndThenTo<TF, TO>(TF failedOutcomeValue, Func<TResult, TO> function);
+        TO AndThenTo<TO>(TimeSpan timeout, Func<TResult, TO> function);
+        TO AndThenTo<TO>(Func<TResult, TO> function);
+
+        ICompletes2<TResult> Otherwise(Func<TResult, TResult> function);
+        ICompletes2<TResult> OtherwiseConsume(Action<TResult> consumer);
+        ICompletes2<TResult> RecoverFrom(Func<Exception, TResult> function);
+
+        TO Await<TO>();
+        TO Await<TO>(TimeSpan timeout);
+        bool IsCompleted { get; }
+        bool HasFailed { get; }
+        void Failed();
+        bool HasOutcome { get; }
+        TResult Outcome { get; }
+        ICompletes2<TResult> Repeat();
+        ICompletes2<TResult> Ready();
+    }
+}

0bfb1673ec9a0d8c403d02690e4b833c95885362
Tomasz Jaskula <thomasv1000@hotmail.fr>
2019-10-04 00:16:15 +0200

    Implement AtomicRefValue

---
+++ src/Vlingo.Common/AtomicRefValue.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) 2012-2019 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using System.Threading;
+
+namespace Vlingo.Common
+{
+    public class AtomicRefValue<T>
+    {
+        private object value;
+        private readonly object defaultValue;
+
+        public AtomicRefValue(T initialValue)
+        {
+            value = initialValue;
+            defaultValue = default(T);
+        }
+
+        public AtomicRefValue()
+            : this(default(T))
+        {
+        }
+
+        public T Get() => (T)Interlocked.CompareExchange(ref value, defaultValue, defaultValue);
+
+        public T Set(T newValue) => (T)Interlocked.Exchange(ref value, newValue);
+
+        public T CompareAndSet(T expected, T update)
+            => (T)Interlocked.CompareExchange(ref value, update, expected);
+    }
+}

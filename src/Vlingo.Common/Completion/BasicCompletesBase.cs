b2dc037b95cd4ddd2d47eb7221f9cb1c320b170d
Tomasz Jaskula <thomasv1000@hotmail.fr>
2019-11-09 00:53:42 +0100

    Add lock to DiagnosticCollector

--- src/Vlingo.Common/Completion/BasicCompletesBase.cs
+++ src/Vlingo.Common/Completion/BasicCompletesBase.cs
@@ -109,29 +109,38 @@ namespace Vlingo.Common.Completion
         private readonly string _baseName;
         private Stopwatch _stopwatch = new Stopwatch();
         private StringBuilder _logs = new StringBuilder();
+        private volatile object syncLock = new object();
         
-
         public DiagnosticCollector(string baseName) => _baseName = baseName;
 
         public void Append(string message) => _logs.AppendLine(message);
 
         public void StartAppend(string message)
         {
-            _stopwatch.Start();
-            _logs.AppendLine($"{_baseName} : {message}");
+            lock (syncLock)
+            {
+                _stopwatch.Start();
+                _logs.AppendLine($"{_baseName} : {message}");
+            }
         }
 
         public void StopAppendStart(string message)
         {
-            _stopwatch.Stop();
-            _logs.AppendLine($"{_baseName} : {message} elapsed time '{_stopwatch.ElapsedMilliseconds}ms' on thread #{System.Threading.Thread.CurrentThread.ManagedThreadId}");
-            _stopwatch.Start();
+            lock (syncLock)
+            {
+                _stopwatch.Stop();
+                _logs.AppendLine($"{_baseName} : {message} elapsed time '{_stopwatch.ElapsedMilliseconds}ms' on thread #{System.Threading.Thread.CurrentThread.ManagedThreadId}");
+                _stopwatch.Start();   
+            }
         }
 
         public void Stop()
         {
-            _stopwatch.Stop();
-            _logs.AppendLine($"{_baseName} : Stopped with total elapsed time '{_stopwatch.ElapsedMilliseconds}ms' on thread #{System.Threading.Thread.CurrentThread.ManagedThreadId}");
+            lock (syncLock)
+            {
+                _stopwatch.Stop();
+                _logs.AppendLine($"{_baseName} : Stopped with total elapsed time '{_stopwatch.ElapsedMilliseconds}ms' on thread #{System.Threading.Thread.CurrentThread.ManagedThreadId}");   
+            }
         }
 
         public string Logs

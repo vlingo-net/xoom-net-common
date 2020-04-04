b8859d39b5ef64676451077b3f8eba52771dd890
Tomasz Jaskula <thomasv1000@hotmail.fr>
2020-04-04 22:47:42 +0200

    Move DateTimeHelper to common

---
+++ src/Vlingo.Common/DateTimeHelper.cs
@@ -0,0 +1,22 @@
+// Copyright © 2012-2020 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using System;
+
+namespace Vlingo.Common
+{
+    public static class DateTimeHelper
+    {
+        private static readonly DateTime Jan1St1970 = new DateTime
+            (1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        
+        public static long CurrentTimeMillis()
+        {
+            return (long) (DateTime.UtcNow - Jan1St1970).TotalMilliseconds;
+        }
+    }
+}

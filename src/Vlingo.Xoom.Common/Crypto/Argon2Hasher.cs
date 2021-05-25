19a25106efc386adf917cdb2ae2b76782057d851
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-05-25 22:57:20 +0200

    Argon2 tests

--- src/Vlingo.Xoom.Common/Crypto/Argon2Hasher.cs
+++ src/Vlingo.Xoom.Common/Crypto/Argon2Hasher.cs
@@ -5,58 +5,27 @@
 // was not distributed with this file, You can obtain
 // one at https://mozilla.org/MPL/2.0/.
 
-using System.Text;
 using Isopoh.Cryptography.Argon2;
-using Isopoh.Cryptography.SecureArray;
 
 namespace Vlingo.Xoom.Common.Crypto
 {
     public class Argon2Hasher : Hasher
     {
-        private readonly Argon2 _argon2;
-        private readonly Argon2Config _argon2Config;
+        private readonly int _maxDuration;
+        private readonly int _memoryCost;
+        private readonly int _parallelism;
 
         public Argon2Hasher(int maxDuration, int memoryCost, int parallelism)
         {
-            _argon2Config = new Argon2Config()
-            {
-                TimeCost = maxDuration,
-                MemoryCost = memoryCost,
-                Lanes = parallelism
-            };
-            _argon2 = new Argon2(_argon2Config);
+            _maxDuration = maxDuration;
+            _memoryCost = memoryCost;
+            _parallelism = parallelism;
         }
         
-        public override string Hash(string plainSecret)
-        {
-            var passwordBytes = Encoding.UTF8.GetBytes(plainSecret);
-            _argon2Config.Password = passwordBytes;
-            using var hashA = _argon2.Hash();
-            return _argon2Config.EncodeString(hashA.Buffer);
-        }
+        public override string Hash(string plainSecret) => 
+            Argon2.Hash(plainSecret, _maxDuration, _memoryCost, _parallelism);
 
-        public override bool Verify(string plainSecret, string hashedSecret)
-        {
-            var passwordBytes = Encoding.UTF8.GetBytes(plainSecret);
-            _argon2Config.Password = passwordBytes;
-            SecureArray<byte>? hashB = null;
-            try
-            {
-                if (_argon2Config.DecodeString(hashedSecret, out hashB) && hashB != null)
-                {
-                    using var hashToVerify = _argon2.Hash();
-                    if (Argon2.FixedTimeEquals(hashB, hashToVerify))
-                    {
-                        return true;
-                    }
-                }
-            }
-            finally
-            {
-                hashB?.Dispose();
-            }
-
-            return false;
-        }
+        public override bool Verify(string plainSecret, string hashedSecret) => 
+            Argon2.Verify(hashedSecret, plainSecret, _parallelism);
     }
 }

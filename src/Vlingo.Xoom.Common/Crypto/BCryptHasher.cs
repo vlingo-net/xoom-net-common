6145898e63a0232c1918639ba8fa6922f4f126a3
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-05-30 08:26:44 +0200

    Add BCrypt hasher

---
+++ src/Vlingo.Xoom.Common/Crypto/BCryptHasher.cs
@@ -0,0 +1,17 @@
+// Copyright © 2012-2021 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+namespace Vlingo.Xoom.Common.Crypto
+{
+    public class BCryptHasher : Hasher
+    {
+        public override string Hash(string plainSecret) => BCrypt.Net.BCrypt.HashPassword(plainSecret);
+
+        public override bool Verify(string plainSecret, string hashedSecret) => 
+            BCrypt.Net.BCrypt.Verify(plainSecret, hashedSecret);
+    }
+}

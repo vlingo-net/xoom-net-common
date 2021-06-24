294221af336fadff8a9928471454d0821dd1418b
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-06-24 19:53:17 +0200

    Formatting

--- src/Vlingo.Xoom.Common/Failure.cs
+++ src/Vlingo.Xoom.Common/Failure.cs
@@ -13,69 +13,39 @@ namespace Vlingo.Xoom.Common
     {
         private readonly TCause _cause;
 
-        internal Failure(TCause cause)
-        {
-            _cause = cause;
-        }
-        
-        public virtual IOutcome<TCause, Tuple<TValue, TSecondSuccess>> AlongWith<TOtherFailure, TSecondSuccess>(IOutcome<TOtherFailure, TSecondSuccess> outcome) where TOtherFailure : Exception
-        {
-            return Failure.Of<TCause, Tuple<TValue, TSecondSuccess>>(_cause);
-        }
+        internal Failure(TCause cause) => _cause = cause;
 
-        public virtual IOutcome<TCause, TNextSuccess> AndThen<TNextSuccess>(Func<TValue, TNextSuccess> action)
-        {
-            return Failure.Of<TCause, TNextSuccess>(_cause);
-        }
+        public virtual IOutcome<TCause, Tuple<TValue, TSecondSuccess>> AlongWith<TOtherFailure, TSecondSuccess>(IOutcome<TOtherFailure, TSecondSuccess> outcome) where TOtherFailure : Exception => 
+            Failure.Of<TCause, Tuple<TValue, TSecondSuccess>>(_cause);
 
-        public virtual IOutcome<TNextFailure, TNextSuccess> AndThenTo<TNextFailure, TNextSuccess>(Func<TValue, IOutcome<TNextFailure, TNextSuccess>> action) where TNextFailure : Exception
-        {
-            return Failure.Of<TNextFailure, TNextSuccess>((TNextFailure)(Exception)_cause);
-        }
+        public virtual IOutcome<TCause, TNextSuccess> AndThen<TNextSuccess>(Func<TValue, TNextSuccess> action) => 
+            Failure.Of<TCause, TNextSuccess>(_cause);
 
-        public virtual ICompletes<TValue> AsCompletes()
-        {
-            return Completes.WithFailure<TValue>();
-        }
+        public virtual IOutcome<TNextFailure, TNextSuccess> AndThenTo<TNextFailure, TNextSuccess>(Func<TValue, IOutcome<TNextFailure, TNextSuccess>> action) where TNextFailure : Exception => 
+            Failure.Of<TNextFailure, TNextSuccess>((TNextFailure)(Exception)_cause);
 
-        public virtual Optional<TValue> AsOptional()
-        {
-            return Optional.Empty<TValue>();
-        }
+        public virtual ICompletes<TValue> AsCompletes() => Completes.WithFailure<TValue>();
+
+        public virtual Optional<TValue> AsOptional() => Optional.Empty<TValue>();
 
         public virtual void AtLeastConsume(Action<TValue> consumer)
         {
         }
 
-        public virtual IOutcome<NoSuchElementException, TValue> Filter(Func<TValue, bool> filterFunction)
-        {
-            return Failure.Of<NoSuchElementException, TValue>(new NoSuchElementException(_cause));
-        }
+        public virtual IOutcome<NoSuchElementException, TValue> Filter(Func<TValue, bool> filterFunction) => 
+            Failure.Of<NoSuchElementException, TValue>(new NoSuchElementException(_cause));
 
-        public virtual TValue Get()
-        {
-            throw _cause;
-        }
+        public virtual TValue Get() => throw _cause;
 
-        public virtual TValue GetOrNull()
-        {
-            return default!;
-        }
+        public virtual TValue GetOrNull() => default!;
 
-        public virtual IOutcome<TCause, TValue> Otherwise(Func<TCause, TValue> action)
-        {
-            return Success.Of<TCause, TValue>(action.Invoke(_cause));
-        }
+        public virtual IOutcome<TCause, TValue> Otherwise(Func<TCause, TValue> action) => Success.Of<TCause, TValue>(action.Invoke(_cause));
 
-        public virtual IOutcome<TNextFailure, TNextSuccess> OtherwiseTo<TNextFailure, TNextSuccess>(Func<TCause, IOutcome<TNextFailure, TNextSuccess>> action) where TNextFailure : Exception
-        {
-            return action.Invoke(_cause);
-        }
+        public virtual IOutcome<TNextFailure, TNextSuccess> OtherwiseTo<TNextFailure, TNextSuccess>(Func<TCause, IOutcome<TNextFailure, TNextSuccess>> action) where TNextFailure : Exception => 
+            action.Invoke(_cause);
 
-        public virtual TNextSuccess Resolve<TNextSuccess>(Func<TCause, TNextSuccess> onFailedOutcome, Func<TValue, TNextSuccess> onSuccessfulOutcome)
-        {
-            return onFailedOutcome.Invoke(_cause);
-        }
+        public virtual TNextSuccess Resolve<TNextSuccess>(Func<TCause, TNextSuccess> onFailedOutcome, Func<TValue, TNextSuccess> onSuccessfulOutcome) => 
+            onFailedOutcome.Invoke(_cause);
 
         public virtual IOutcome<TNextFailure, TValue> OtherwiseFail<TNextFailure>(Func<TCause, TNextFailure> action) 
             where TNextFailure : Exception
@@ -101,9 +71,7 @@ namespace Vlingo.Xoom.Common
 
     public static class Failure
     {
-        public static IOutcome<TCause, TValue> Of<TCause, TValue>(TCause cause) where TCause : Exception
-        {
-            return new Failure<TCause, TValue>(cause);
-        }
+        public static IOutcome<TCause, TValue> Of<TCause, TValue>(TCause cause) where TCause : Exception => 
+            new Failure<TCause, TValue>(cause);
     }
 }

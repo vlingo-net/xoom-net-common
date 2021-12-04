dc134532272975263377e2c8f941a78de36eb7d1
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-12-04 13:14:19 +0100

    Move DateExtensions from Http project to Common

---
+++ src/Vlingo.Xoom.Common/DateExtensions.cs
@@ -0,0 +1,35 @@
+// Copyright (c) 2012-2021 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using System;
+
+namespace Vlingo.Xoom.Common
+{
+    public static class DateExtensions
+    {
+        public static double GetCurrentMillis()
+        {
+            var jan1970 = new DateTime(1970, 1, 1, 0, 0,0, DateTimeKind.Utc);
+            var javaSpan = DateTime.UtcNow - jan1970;
+            return javaSpan.TotalMilliseconds;
+        }
+
+        public static double GetMillis(this DateTime date)
+        {
+            var jan1970 = new DateTime(1970, 1, 1, 0, 0,0, DateTimeKind.Utc);
+            var javaSpan = date - jan1970;
+            return javaSpan.TotalMilliseconds;
+        }
+        
+        public static long GetCurrentSeconds(this DateTime dateTime)
+        {
+            var jan1970 = new DateTime(1970, 1, 1, 0, 0,0, DateTimeKind.Utc);
+            var javaSpan = dateTime - jan1970;
+            return Convert.ToInt64(javaSpan.TotalSeconds);
+        }
+    }
+}
